95b3c39dd05d17e3512471aab19f7fe90c3e4893
one <belahcensymow@gmail.com>
2026-07-03 02:21:14 +0100

    The camera is working correctly now. Praise GOD!

--- Camera.cpp
+++ Camera.cpp
@@ -14,25 +14,19 @@ Camera::Camera()
     SRT(Scale, Rotation, Position, ABSOLUTE);
 
 }
-void Camera::updateView(Shader &shader)
-{
-    std::array<float, 3> translationCorrection = {1.0f, -1.0f, 1.0f};
-    std::array<float, 3> rotationCorrection = {-1.0f, 1.0f, -1.0f};
-    View = ::SRT(Scale * -1.0f, Rotation * rotationCorrection, Position * translationCorrection);
-    applySRT(shader);
-}
 void Camera::getInput(Shader &shader, GLFWwindow *window)
 {
-    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) translate(0,0,cameraSpeed,RELATIVE);
-    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) translate(0,0,-cameraSpeed,RELATIVE);
-    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) translate(cameraSpeed,0,0,RELATIVE);
-    if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) translate(-cameraSpeed,0,0, RELATIVE);
-    if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)   rotate( cameraSpeed*5,0,0,RELATIVE);
-    if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) rotate(-cameraSpeed*5,0,0,RELATIVE);
-    if(glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)rotate(0, cameraSpeed*5,0,RELATIVE);
-    if(glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) rotate(0,-cameraSpeed*5,0,RELATIVE);
-    updateView(shader);
+    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) goForward();
+    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) goBackward();
+    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) goLeft();
+    if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) goRight();
+    if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)    lookUp(2.0f);
+    if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)  lookDown(2.0f);
+    if(glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) lookRight(5.0f);
+    if(glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)  lookLeft(5.0f);
+    applyView(shader);
 }
+
 std::array<float, 3>Camera::getPosition() { return Position; }
 std::array<float, 3> Camera::getDirection(std::array<float, 3> targetPosition) { return Normalize(Position - targetPosition); }
 std::array<float, 3> Camera::getRight(std::array<float, 3> targetPosition) { return Normalize(crossProduct3({0, 1, 0}, getDirection(targetPosition))); }
@@ -44,14 +38,15 @@ std::array<float, 3> Camera::lookAt(std::array<float, 3> targetPosition)
     return  { std::asin(F[1]), std::atan2(F[2],F[0]), 0.0f };
 }
 
-void Camera::applySRT(Shader &shader, std::array<float, 16> SRTMatrix, std::string uniformName)
+void Camera::applyView(Shader &shader, std::string uniformName)
 {
     shader.bind();
+    View = ViewMatrix();
     int location = glGetUniformLocation(shader.program, uniformName.c_str());
-    glUniformMatrix4fv(location, 1, GL_FALSE, SRTMatrix.data());
+    glUniformMatrix4fv(location, 1, GL_FALSE, View.data());
     shader.unbind();
 }
-void Camera::applySRT(Shader &shader) { applySRT(shader ,View, "View");}
+void Camera::applyView(Shader &shader) { applyView(shader, "View");}
 
 void Camera::SRT(float scaleX, float scaleY, float scaleZ, float rotateX, float rotateY, float rotateZ, float translateX, float translateY, float translateZ, TRANSFORMATION_TYPE transformationType)
 {
@@ -93,3 +88,60 @@ void Camera::translate(float x, float y, float z, TRANSFORMATION_TYPE transforma
     if(transformationType == ABSOLUTE) Position = position;
     else Position += position;
 }
+
+std::array<float, 3> Camera::getTargetPosition()
+{
+    float radPitch = Rotation[0] * 3.14159265f / 180.0f;
+    float radYaw   = Rotation[1] * 3.14159265f / 180.0f;
+    return Position + Normalize({
+        std::cos(radYaw) * std::cos(radPitch),
+        std::sin(radPitch),
+        std::sin(radYaw) * std::cos(radPitch)
+    });
+}
+std::array<float, 16> Camera::ViewMatrix()
+{
+    std::array<float, 3> targetPosition = getTargetPosition();
+    std::array<float, 3> D = getDirection(targetPosition);
+    std::array<float, 3> R = getRight(targetPosition);
+    std::array<float, 3> U = getUp(targetPosition);
+    float sx = (Scale[0] != 0.0f) ? (1.0f / Scale[0]) : 1.0f;
+    float sy = (Scale[1] != 0.0f) ? (1.0f / Scale[1]) : 1.0f;
+    float sz = (Scale[2] != 0.0f) ? (1.0f / Scale[2]) : 1.0f;
+    R *= sx; U *= sy; D *= sz;
+    float transX = -dotProduct3(R, Position);
+    float transY = -dotProduct3(U, Position);
+    float transZ = -dotProduct3(D, Position);
+    return {
+        R[0],   U[0],   D[0],   0.0f,
+        R[1],   U[1],   D[1],   0.0f,
+        R[2],   U[2],   D[2],   0.0f,
+        transX, transY, transZ, 1.0f
+    };
+}
+
+
+void Camera::goForward(float speed)
+{
+    std::array<float, 3> M = getForward(getTargetPosition()) * speed;
+    Position -= M;
+}
+void Camera::goBackward(float speed)
+{
+    std::array<float, 3> M = getForward(getTargetPosition()) * speed;
+    Position += M;
+}
+void Camera::goRight(float speed)
+{
+    std::array<float, 3> M = getRight(getTargetPosition()) * speed;
+    Position += M;
+}
+void Camera::goLeft(float speed)
+{
+    std::array<float, 3> M = getRight(getTargetPosition()) * speed;
+    Position -= M;
+}
+void Camera::lookUp(float speed) { Rotation[0] += speed; }
+void Camera::lookDown(float speed) { Rotation[0] -= speed; }
+void Camera::lookRight(float speed) { Rotation[1] += speed; }
+void Camera::lookLeft(float speed) { Rotation[1] -= speed; }
--- Camera.h
+++ Camera.h
@@ -14,7 +14,6 @@ class Camera
         std::array<float, 3> Scale;
         std::array<float, 16> View;
         Camera();
-        void updateView(Shader &shader);
         void getInput(Shader &shader, GLFWwindow *window);
         std::array<float, 3> getPosition();
         std::array<float, 3> getDirection(std::array<float, 3> targetPosition);
@@ -27,12 +26,24 @@ class Camera
 
 
 
-        void applySRT(Shader &shader, std::array<float, 16> SRTMatrix, std::string uniformName);
-        void applySRT(Shader &shader);
+        void applyView(Shader &shader, std::string uniformName);
+        void applyView(Shader &shader);
         void SRT(float scaleX, float scaleY, float scaleZ, float rotateX, float rotateY, float rotateZ, float translateX, float translateY, float translateZ, TRANSFORMATION_TYPE transformationType = ABSOLUTE);
         void SRT(std::array<float, 3> scale, std::array<float, 3> rotation, std::array<float, 3> translation, TRANSFORMATION_TYPE transformationType);
         void scale(float x, float y, float z, TRANSFORMATION_TYPE transformationType = ABSOLUTE);
         void rotate(float x, float y, float z, TRANSFORMATION_TYPE transformationType = ABSOLUTE);
         void translate(float x, float y, float z, TRANSFORMATION_TYPE transformationType = ABSOLUTE);
+
+        std::array<float, 3> getTargetPosition();
+        std::array<float, 16> ViewMatrix();
+
+        void goForward(float speed = 0.05f);
+        void goBackward(float speed = 0.05f);
+        void goRight(float speed = 0.05f);
+        void goLeft(float speed = 0.05f);
+        void lookUp(float speed = 0.05f);
+        void lookDown(float speed = 0.05f);
+        void lookRight(float speed = 0.05f);
+        void lookLeft(float speed = 0.05f);
 
 };
